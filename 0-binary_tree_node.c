d7397c86f7d40a69fc05ef6ad7978d72886e6cbc
yichehnc <yichenc2017@gmail.com>
2024-01-16 17:15:35 +1100

    fix: c:17 trailing whitespace

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == 0)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
