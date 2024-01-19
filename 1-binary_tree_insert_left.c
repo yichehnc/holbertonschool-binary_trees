e6062c493a5c9ca24ba3a79f0d9fb343d195ea68
yichehnc <yichenc2017@gmail.com>
2024-01-19 18:11:26 +1100

    feat: Write a function that inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	else  
+	else
 	{
 		new_node->left = NULL;
 	}
