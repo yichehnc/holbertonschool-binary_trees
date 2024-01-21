ff096c6477e385241e7cea8be1739eb01445abf5
yichehnc <yichenc2017@gmail.com>
2024-01-21 22:04:36 +1100

    feat: Write a function that goes through a binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,10 +9,18 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func != NULL)
-		return;
+	if (tree != NULL && func != NULL)
+	{
+		if (tree->left != NULL)
+		{
+			binary_tree_postorder(tree->left, func);
+		}
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+		if (tree->right != NULL)
+		{
+			binary_tree_postorder(tree->right, func);
+		}
+
+		func(tree->n);
+	}
 }
